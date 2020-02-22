7a8217446e8fc4b9b51a6ec474825e2bad67b64c
LucianABC <louabecasis@gmail.com>
2020-02-22 20:59:57 -0300

    Updates

---
+++ precache-manifest.1e1d7525ea51213a37bab7b793d36ce7.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "beaea41224e4a4d5ced79a0fdd692fbb",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "abd94f0fdacb76178728",
+    "url": "/Portfolio/static/css/main.dc176486.chunk.css"
+  },
+  {
+    "revision": "a2cf9fafd07d60997f30",
+    "url": "/Portfolio/static/js/2.849ed816.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/Portfolio/static/js/2.849ed816.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abd94f0fdacb76178728",
+    "url": "/Portfolio/static/js/main.db298ef7.chunk.js"
+  },
+  {
+    "revision": "38e9353468597077cd3e",
+    "url": "/Portfolio/static/js/runtime-main.ed271e06.js"
+  },
+  {
+    "revision": "41590b75954e61d42d185caac3a77248",
+    "url": "/Portfolio/static/media/breakfastclub.41590b75.png"
+  },
+  {
+    "revision": "5a378ddf7bb1e4ce77883f0114c886dd",
+    "url": "/Portfolio/static/media/cellphone.5a378ddf.png"
+  },
+  {
+    "revision": "ad79130f64a99a9fc3c473fd0945cb41",
+    "url": "/Portfolio/static/media/checkeredbackground.ad79130f.jpg"
+  },
+  {
+    "revision": "1f76d8ffcfc887d96d645619960f954e",
+    "url": "/Portfolio/static/media/coding.1f76d8ff.png"
+  },
+  {
+    "revision": "59a5aaf0eb4556c97972fbf05bb0d861",
+    "url": "/Portfolio/static/media/foto2.59a5aaf0.jpg"
+  },
+  {
+    "revision": "2f0c4fe5085b7d8e1bae8d8f335ba8e9",
+    "url": "/Portfolio/static/media/gmail.2f0c4fe5.png"
+  },
+  {
+    "revision": "e0695d69c143271f655355c456187f55",
+    "url": "/Portfolio/static/media/linkedin.e0695d69.png"
+  },
+  {
+    "revision": "b4097366b2447512d8431cf4bdc9b9bf",
+    "url": "/Portfolio/static/media/matrix.b4097366.png"
+  },
+  {
+    "revision": "50217ca2ab0f80579343c56e3a7892bf",
+    "url": "/Portfolio/static/media/microphone.50217ca2.png"
+  },
+  {
+    "revision": "77681bfb20dd78ad655f8928157233f6",
+    "url": "/Portfolio/static/media/nirvana.77681bfb.png"
+  },
+  {
+    "revision": "beb97f95a05f17931999c9f3de091fb2",
+    "url": "/Portfolio/static/media/orange.beb97f95.png"
+  },
+  {
+    "revision": "c5786dd38d192911ba5a4afa460f65ee",
+    "url": "/Portfolio/static/media/paperwallpaper.c5786dd3.jpg"
+  },
+  {
+    "revision": "5d1c5fcbf543c2dbc3188182e7c43207",
+    "url": "/Portfolio/static/media/phone-call.5d1c5fcb.png"
+  },
+  {
+    "revision": "a9eb51b1dccbeac8f483a06727eb6487",
+    "url": "/Portfolio/static/media/polaroidframe2.a9eb51b1.png"
+  },
+  {
+    "revision": "783267fd55df435253a775ae9c5d8375",
+    "url": "/Portfolio/static/media/postit1.783267fd.png"
+  },
+  {
+    "revision": "a9c84cf50f2e7e217bcbbd079f7e35b3",
+    "url": "/Portfolio/static/media/postit2.a9c84cf5.png"
+  },
+  {
+    "revision": "7bc7abfbe7797e0abe3e4da35321792b",
+    "url": "/Portfolio/static/media/postit3.7bc7abfb.png"
+  },
+  {
+    "revision": "15fece9d4950ceda149d79d249132776",
+    "url": "/Portfolio/static/media/postit4.15fece9d.png"
+  },
+  {
+    "revision": "399928f8a29d7d9536494fe4905d019a",
+    "url": "/Portfolio/static/media/postit5.399928f8.png"
+  },
+  {
+    "revision": "2bc90ae936976b7649215a7532736964",
+    "url": "/Portfolio/static/media/postit6.2bc90ae9.png"
+  },
+  {
+    "revision": "20849fa67aa94aefd718226d08671e15",
+    "url": "/Portfolio/static/media/ravenclaw.20849fa6.png"
+  },
+  {
+    "revision": "1eadd588d33f91f06a89c769f94b866e",
+    "url": "/Portfolio/static/media/title.1eadd588.png"
+  },
+  {
+    "revision": "aad9bd117b6b90ed11d76bf81a8afa4b",
+    "url": "/Portfolio/static/media/woodbackground.aad9bd11.jpg"
+  }
+]);
